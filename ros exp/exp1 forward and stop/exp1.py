641f2f52a7ccf1619b97a904f5ec2f3be8b37096
guestlol123 <22bb15181@rit.ac.in>
2024-10-16 01:39:16 -0700

    Update exp1.py

--- ros exp/exp1 forward and stop/exp1.py
+++ ros exp/exp1 forward and stop/exp1.py
@@ -1,3 +1,22 @@
-create a workspace
-create package with dependencies rospy,geometry_msgs
-rosrun <package_name> <pythonfile_name.py>
+# Import ROS libraries
+import rospy
+from sensor_msgs.msg import LaserScan
+
+# Callback function to process laser scan data
+def callback(data):
+    # data.ranges contains the distances measured by the laser scanner
+    min_distance = min(data.ranges)
+    rospy.loginfo("Minimum distance to an obstacle: {:.2f} meters".format(min_distance))
+
+def listener():
+    # Initialize the ROS node
+    rospy.init_node('obstacle_distance_listener', anonymous=True)
+
+    # Subscribe to the LaserScan topic
+    rospy.Subscriber('/scan', LaserScan, callback)
+
+    # Keep the node running
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
